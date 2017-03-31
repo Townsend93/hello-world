8e7cd8cdb9554360e67691b9dcb5db1daaacebd1
@tancheng <8243786052@qq.com>
2017-03-31 16:05:22 +0800

    add

---
+++ SwiftPro/SwiftProTests/SwiftProTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftProTests.swift
+//  SwiftProTests
+//
+//  Created by 谭谭诚 on 2017/3/31.
+//  Copyright © 2017年 tancheng. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftPro
+
+class SwiftProTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
